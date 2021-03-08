663ae5a0f49e4b91609d7694b6383d76aa43d545
Yerbolat <twinspazyl@gmail.com>
2021-03-08 23:12:57 +0600

    doing lab5#2

--- ead3-ass-1/src/main/java/kz/iitu/spring/demo_atm/service/impl/EmployeeServiceImpl.java
+++ ead3-ass-1/src/main/java/kz/iitu/spring/demo_atm/service/impl/EmployeeServiceImpl.java
@@ -1,8 +1,14 @@
 package kz.iitu.spring.demo_atm.service.impl;
 
 import kz.iitu.spring.demo_atm.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repository.EmployeeRepository;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
 }
